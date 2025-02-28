2614223f1ecf2836c61d6687e1e276fd68804cf4
hermonochy <hermonochy@users.noreply.github.com>
2025-02-28 17:18:12 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,57 @@
+# QuizMasterPaper
+
+QuizMasterPaper is a simple GUI-based quiz creator application to export the quizzes in either PDF or TXT formats for printing. Quizzes from other QuizMaster applications are an acceptable format. 
+
+![](images/Screenshot1.png)
+
+## Installation
+
+1. Clone this repository via terminal ```git clone https://github.com/hermonochy/QuizMasterPaper```
+2. Enter the directory containing the game executable: ```cd QuizMasterPaper```
+3. Set up a new virtual environment: ```python3 -m venv venv```
+4. Activate the environment: ```source venv/bin/activate``` (To decativate, type `deactivate`)
+5. Install tkinter: ```sudo apt-get install python3-tk```
+6. Install packages in `requirements.txt`: ```pip3 install -r requirements.txt```
+
+*Steps 3 and 4 are optional but recommended.*
+
+
+## Usage
+
+1. Run the Application: Start the application by running the script:
+```sh
+    python3 quizcreator.py
+```
+2. Create a New Quiz:
+ Enter the title of the quiz in the "Title of Quiz" field.
+ Add questions using the "Add Question" button.
+ Fill in the question, the correct answer, and wrong answers when prompted.
+ Repeat this process to add multiple questions.
+
+3. Edit Questions:
+    Select a question from the list.
+    Click "Edit Question" to modify the selected question.
+
+4. Delete Questions:
+    Select a question and click "Delete Question" to remove it from the list.
+
+5. Save the Quiz:
+    Choose a format (PDF or TXT) using the radio buttons.
+    Click "Save" to export your quiz.
+
+6. Open an Existing Quiz:
+    Click "Open" to browse and load a quiz from a JSON file.
+
+### Features
+
+- Create and manage quiz questions.
+- Add correct and wrong answers for each question.
+- Edit existing questions.
+- Delete questions from the quiz.
+- Save quizzes as PDF or TXT files.
+- Load existing quizzes from JSON files.
+
+## Future work
+
+- More saving formats
+- More flexible layout
---
+++ quizcreator.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+import PySimpleGUI as sg
+import json
+from dataclasses import dataclass, field
+from typing import List
+from fpdf import FPDF
+
+@dataclass
+class QuizQuestion:
+    question: str
+    correctAnswer: str
+    wrongAnswers: List[str]
+    timeout: int = field(default=10)
+
+    def __repr__(self):
+        return self.question
+
+questionList = []
+
+def create_pdf(quiz_title, questions):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    pdf.cell(200, 10, txt=quiz_title, ln=True, align='C')
+
+    for i, question in enumerate(questions):
+        pdf.cell(200, 10, txt=f"{i + 1}. {question.question}", ln=True)
+        pdf.cell(200, 10, txt=f"  A) {question.correctAnswer}", ln=True)
+        for j, wrong_answer in enumerate(question.wrongAnswers):
+            pdf.cell(200, 10, txt=f"  {chr(66 + j)}) {wrong_answer}", ln=True)
+        pdf.cell(200, 10, txt="", ln=True)
+
+    pdf_file_path = f"output/{quiz_title}.pdf"
+    pdf.output(pdf_file_path)
+    sg.popup("Quiz saved as PDF!", title="Success")
+    
+def save_as_txt(quiz_title, questions):
+    with open(f"output/{quiz_title}.txt", 'w') as f:
+        f.write(f"{quiz_title}\n")
+        for i, question in enumerate(questions):
+            f.write(f"{i + 1}. {question.question}\n")
+            f.write(f"  A) {question.correctAnswer}\n")
+            for j, wrong_answer in enumerate(question.wrongAnswers):
+                f.write(f"  {chr(66 + j)}) {wrong_answer}\n")
+            f.write("\n")
+    sg.popup("Quiz saved as TXT!", title="Success")
+
+mainWinowLayout = [
+    [sg.Text("Title of Quiz:"), sg.InputText(key='quiz_name')],
+    [sg.Listbox(questionList, size=(125, 25), key='quizquestionentry', enable_events=True), sg.Text(key='textbox', size=(20, 10))],
+    [sg.Button('Open'), sg.Button('Save'), sg.Button("Edit Question"), sg.Button("Add Question"), sg.Button("Delete Question")],
+    [sg.Radio('PDF', 'OUTPUT_FORMAT', key='pdf', default=True), sg.Radio('TXT', 'OUTPUT_FORMAT', key='txt')],
+    [sg.Button('Quit')]
+]
+
+mainWindow = sg.Window('Quiz Creator', mainWinowLayout)
+
+def make_questionEditorWindow():
+    questionEditorLayout = [
+        [sg.Text('Enter the question:'), sg.InputText(key='question')],
+        [sg.Text('Enter the correct answer:'), sg.InputText(key='correct_answer')],
+        [sg.Text('Enter the wrong answers:'), sg.InputText(key='wrong_answers')],
+        [sg.Button('Add'), sg.Button('Cancel')]
+    ]
+    
+    questionEditorWindow = sg.Window('Question Editor', questionEditorLayout, finalize=True)
+    return questionEditorWindow
+
+while True:
+    event, values = mainWindow.read()
+    
+    if event == 'quizquestionentry':
+        if len(values['quizquestionentry']) > 0:
+            answers = ("correct answer:", values['quizquestionentry'][0].correctAnswer, "wrong answers:", values['quizquestionentry'][0].wrongAnswers)
+            outputtext = "Correct answer: \n" + answers[1] + "\n Wrong answers:\n" + str(answers[3])
+            mainWindow["textbox"].update(outputtext)
+    
+    if event == sg.WIN_CLOSED or event == 'Quit':
+        print("Bye...")
+        break
+
+    if event == 'Edit Question':
+        try:
+            index = int(''.join(map(str, mainWindow["quizquestionentry"].get_indexes())))
+            quizQuestion = questionList[index]
+        except ValueError:
+            sg.Popup("Select a question to edit!")
+            continue
+
+        questionEditorWindow = make_questionEditorWindow()
+        questionEditorWindow['question'].update(quizQuestion.question)
+        questionEditorWindow['correct_answer'].update(quizQuestion.correctAnswer)
+        questionEditorWindow['wrong_answers'].update(', '.join(quizQuestion.wrongAnswers))
+        
+        editorEvent, editorValues = questionEditorWindow.read()
+        if editorEvent == 'Add':
+            question = editorValues['question']
+            correct_answer = editorValues['correct_answer']
+            wrong_answers = editorValues['wrong_answers'].split(',')
+            newquestion = QuizQuestion(question, correct_answer, wrong_answers)
+            questionList[index] = newquestion  
+            questionEditorWindow.close()
+            mainWindow["quizquestionentry"].update(questionList)
+        if editorEvent == 'Cancel':
+            questionEditorWindow.close()
+
+    if event == 'Add Question':
+        questionEditorWindow = make_questionEditorWindow()
+        editorEvent, editorValues = questionEditorWindow.read()
+        if editorEvent == 'Add':
+            question = editorValues['question']
+            correct_answer = editorValues['correct_answer']
+            wrong_answers = editorValues['wrong_answers'].split(',')
+            newquestion = QuizQuestion(question, correct_answer, wrong_answers)
+            questionList.append(newquestion)
+            questionEditorWindow.close()
+            mainWindow["quizquestionentry"].update(questionList)
+        if editorEvent == 'Cancel':
+            questionEditorWindow.close()
+        
+    if event == 'Save':
+        quiz_title = values['quiz_name']
+        if values['pdf']:
+            create_pdf(quiz_title, questionList)
+        elif values['txt']:
+            save_as_txt(quiz_title, questionList)
+
+    if event == 'Open':
+        try:
+            filename = sg.popup_get_file('Open', no_window=True, initial_folder="quizzes",
+                                          file_types=(("All JSON Files", "*.json"), ("All Files", "*.*")))
+            with open(filename, 'r') as file:
+                quizDicts = json.load(file)
+                questionList = []
+                for q in quizDicts["listOfQuestions"]:
+                    qq = QuizQuestion(**q)
+                    questionList.append(qq)
+                mainWindow["quizquestionentry"].update(questionList)
+                titleofquiz = quizDicts["title"]
+                mainWindow["quiz_name"].update(titleofquiz)
+        except TypeError:
+            pass
+    
+    if event == 'Delete Question':
+        try:
+            index = int(''.join(map(str, mainWindow["quizquestionentry"].get_indexes())))
+            questionList.pop(index)
+        except ValueError:
+            sg.Popup("Select a question to delete!")
+
+        mainWindow["quizquestionentry"].update(questionList)
+
+mainWindow.close()
+
